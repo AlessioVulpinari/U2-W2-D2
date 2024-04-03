6d3a331eb2ceaef4147b629d2c96db25a4b2782f
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-04-03 20:08:58 +0200

    Ex 17

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,24 @@
-const collapseElementList = document.querySelectorAll(".collapse")
-const collapseList = [...collapseElementList].map(
-  (collapseEl) => new bootstrap.Collapse(collapseEl)
-)
+window.onload = function () {
+  const collapseElementList = document.querySelectorAll(".collapse")
+  const collapseList = [...collapseElementList].map(
+    (collapseEl) => new bootstrap.Collapse(collapseEl)
+  )
 
-const myModal = document.getElementById("modal")
-const myInput = document.getElementById("btnContatti")
+  const myModal = document.getElementById("modal")
+  const myInput = document.getElementById("btnContatti")
 
-myModal.addEventListener("shown.bs.modal", () => {
-  myInput.focus()
-})
+  myModal.addEventListener("shown.bs.modal", () => {
+    myInput.focus()
+  })
+
+  const removeCards = () => {
+    const cards = document.querySelectorAll(".card")
+
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i]
+      card.remove()
+    }
+  }
+
+  // removeCards()
+}
