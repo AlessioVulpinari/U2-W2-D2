2e3741638fb67af754581e1b778dcf5beff1b0df
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-04-03 19:31:56 +0200

    Esercizio 14 e 15

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,10 @@ const collapseElementList = document.querySelectorAll(".collapse")
 const collapseList = [...collapseElementList].map(
   (collapseEl) => new bootstrap.Collapse(collapseEl)
 )
+
+const myModal = document.getElementById("modal")
+const myInput = document.getElementById("btnContatti")
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus()
+})
